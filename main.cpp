8dc100d583a81ac3adffc6749c3e3f6eff997946
nishi-gap <gap298aka@gmail.com>
2023-08-16 13:56:12 +0900

    update

--- main.cpp
+++ main.cpp
@@ -1,11 +1,7 @@
 #include "widget.h"
-#include "setrulings.h"
-#include <glm/glm.hpp>
-#include <numbers>
 #include <QApplication>
 #include <iomanip>
-#include <glm/gtx/vector_angle.hpp>
-#include <mathtool.h>
+
 //#include <QSurfaceFormat>
 //https://www.bogotobogo.com/Qt/Qt5_OpenGL_QGLWidget.php
 
